e5fcd6c3e9af7536ac13d6744da1e2fd68d03898
daan273 <daan.eeltink@gmail.com>
2020-07-31 14:48:34 +0200

    Z safe homing location

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1389,8 +1389,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT 50  // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT 15  // Y point for Z homing
 #endif
 
 // Homing speeds (mm/m)
